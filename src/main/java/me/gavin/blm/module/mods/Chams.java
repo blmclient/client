9cacd8809afcf7f4aebad8702b8dd1fad4f861a6
Gavin <72846934+Gav06@users.noreply.github.com>
2021-09-09 20:32:33 -0400

    fix chams a lil bit

--- src/main/java/me/gavin/blm/module/mods/Chams.java
+++ src/main/java/me/gavin/blm/module/mods/Chams.java
@@ -14,28 +14,38 @@ import static org.lwjgl.opengl.GL11.*;
 )
 public final class Chams extends Module {
 
-    private final ModeSetting<>
+    private final ModeSetting<ChamsMode> chamsMode = new ModeSetting<>("Mode", ChamsMode.Default);
 
     @SubscribeEvent
     public void onRenderEntityModel(RenderEntityModelEvent event) {
         event.setCanceled(true);
 
-        glLineWidth(1.5f);
+        if (chamsMode.getValue() == ChamsMode.XQZ) {
 //        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glColor3f(1f, 0f, 0f);
-        glDisable(GL_TEXTURE_2D);
-        glDisable(GL_DEPTH_TEST);
-        glDisable(GL_LIGHTING);
-        render(event);
-        glEnable(GL_LIGHTING);
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_TEXTURE_2D);
+            glColor3f(1f, 0f, 0f);
+            glDisable(GL_TEXTURE_2D);
+            glDisable(GL_DEPTH_TEST);
+            glDisable(GL_LIGHTING);
+            render(event);
+            glEnable(GL_LIGHTING);
+            glEnable(GL_DEPTH_TEST);
+            glEnable(GL_TEXTURE_2D);
 //        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        glColor4f(1f, 1f, 1f, 1f);
-        render(event);
+            glColor4f(1f, 1f, 1f, 1f);
+            render(event);
+        } else if (chamsMode.getValue() == ChamsMode.Default) {
+            glDepthRange(0, 0.01);
+            render(event);
+            glDepthRange(0, 1.0);
+        }
     }
 
     private void render(RenderEntityModelEvent event) {
         event.getModelBase().render(event.getEntity(), event.getLimbSwing(), event.getLimbSwingAmount(), event.getAgeInTicks(), event.getNetHeadYaw(), event.getHeadPitch(), event.getScaleFactor());
+    }
+
+    public enum ChamsMode {
+        Default,
+        XQZ
     }
 }
